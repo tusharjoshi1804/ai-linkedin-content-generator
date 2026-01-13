99226c6990546291196147c8eb7ce456a244bbf7
tusharjoshi1804 <159800796+tusharjoshi1804@users.noreply.github.com>
2026-01-13 17:59:16 +0530

    Create post_generator.py

---
+++ post_generator.py
@@ -0,0 +1,56 @@
+from llm_helper import llm
+from few_shot import FewShotPosts
+
+few_shot = FewShotPosts()
+
+
+def get_length_str(length):
+    if length == "Short":
+        return "1 to 5 lines"
+    if length == "Medium":
+        return "6 to 10 lines"
+    if length == "Long":
+        return "11 to 15 lines"
+
+
+def generate_post(length, language, tag):
+    prompt = get_prompt(length, language, tag)
+    response = llm.invoke(prompt)
+    return response.content
+
+
+def get_prompt(length, language, tag):
+    length_str = get_length_str(length)
+
+    prompt = f'''
+Generate a professional LinkedIn post using the below information. No preamble.
+
+1) Topic: {tag}
+2) Length: {length_str}
+3) Language: {language}
+
+Rules:
+- If Language is Hinglish, mix Hindi and English.
+- If Language is French, generate the post fully in French.
+- If Language is Spanish, generate the post fully in Spanish.
+- Otherwise generate in English.
+'''
+    # prompt = prompt.format(post_topic=tag, post_length=length_str, post_language=language)
+
+    examples = few_shot.get_filtered_posts(length, language, tag)
+
+    if len(examples) > 0:
+        prompt += "4) Use the writing style as per the following examples."
+
+    for i, post in enumerate(examples):
+        post_text = post['text']
+        prompt += f'\n\n Example {i+1}: \n\n {post_text}'
+
+        if i == 1: # Use max two samples
+            break
+
+    return prompt
+
+
+if __name__ == "__main__":
+    print(generate_post("Medium", "English", "Mental Health"))
